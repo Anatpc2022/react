36862d247b19b503d32aa1353421fe10801d2a82
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-13 00:47:50 -0300

    Evento com função

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,8 +1,17 @@
 const Events = () => {
+
+const handleClick = (e) => {
+    console.log(e)
+    console.log("Executou!")
+}
+
   return (
     <div>
         <div>
             <button onClick={() => console.log("Testando um evento.")}>Clique Aqui!</button>
+        </div>
+        <div>
+            <button onClick={handleClick}>Clique Aqui com função</button>
         </div>
     </div>
   )
