310094c26d06aafb8f1f4f1272574fc66d6b48d0
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-13 10:41:16 -0300

    A propriedade key

--- 2_FUNDAMENTOS/fundamentos/src/components/ListRender.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/ListRender.jsx
@@ -3,14 +3,27 @@ import { useState } from "react"
 
 const ListRender = () => {
 
-    const [list] = useState(["Ana", "Lanna", "Nilson Jr.", "Felipe"]);
+   const [list] = useState(["Ana", "Lanna", "Nilson Jr.", "Felipe", "Ana"]);
+
+    const [users, setUsers] = useState([
+      {id: 1, name: "Ana", age: 43},
+      {id: 2, name: "Felipe", age: 24},
+      {id: 3, name: "Nilson jr.", age: 23},
+      {id: 4, name: "Lanna", age: 17}
+    ])
 
   return (
     <div>
-        <ul>
-           {list.map((item) => (
-            <li>{item}</li>
+         <ul>
+           {list.map((item, i) => (
+            <li key={i}>{item}</li>
            ))} 
+        </ul>
+        {/*render com key */}
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name} - {user.age} anos</li>
+          ))}
         </ul>
     </div>
   )
