9af19fd5e8dd9e1bbcdc1a13268436019d828292
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-13 00:11:56 -0300

    Importando componentes

--- 2_FUNDAMENTOS/fundamentos/src/App.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import FirstComponent from './components/FirstComponents';
 
 function App() {
 
-
   return (    
       <div className='App'>
-     
+        <h1>Fundamentos do React</h1>
+        <FirstComponent/>
       </div>   
   )
 }
