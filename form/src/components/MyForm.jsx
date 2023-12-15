47970da4fe57a5de91f036cefd830a91023ca649
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-15 10:24:08 -0300

    Label envolvendo input

--- form/src/components/MyForm.jsx
+++ form/src/components/MyForm.jsx
@@ -1,18 +1,23 @@
 import "./MyForm.css";
 
 const MyForm = () => {
-  return ( <div>
-    {/*1 - Criação de formulário */}
-    <form>
-        <div>
-            <label htmlFor="name">Nome:</label>
-            <input type="text" name="name" placeholder="Digite o seu nome" />
-        </div>
-        <input type="submit" value="Enviar" />
-    </form>
+    return (<div>
+        {/*1 - Criação de formulário */}
+        <form>
+            <div>
+                <label htmlFor="name">Nome:</label>
+                <input type="text" name="name" placeholder="Digite o seu nome" />
+            </div>
+            {/*1 - Label envolvendo input */}
+            <label>
+                <span>E-mail</span>
+                <input type="email" name="email" placeholder="Digite seu email" />
+            </label>
+            <input type="submit" value="Enviar" />
+        </form>
 
     </div>
-  )
+    )
 }
 
 export default MyForm
