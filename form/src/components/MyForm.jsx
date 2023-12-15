9898f4276e963800660c961c3233c7263acc540e
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-15 10:40:35 -0300

    Simplificando a manipulação

--- form/src/components/MyForm.jsx
+++ form/src/components/MyForm.jsx
@@ -9,19 +9,30 @@ const MyForm = () => {
     const handleName = (e) => {
        setName(e.target.value); 
     }
-    console.log(name);
+    console.log(name, email);
 
     return (<div>
         {/*1 - Criação de formulário */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite o seu nome"onChange={handleName}/>
+                <input 
+                type="text" 
+                name="name" 
+                placeholder="Digite o seu nome"
+                onChange={handleName}
+                />
             </div>
             {/*2 - Label envolvendo input */}
             <label>
                 <span>E-mail</span>
-                <input type="email" name="email" placeholder="Digite seu email" />
+                <input 
+                type="email" 
+                name="email" 
+                placeholder="Digite seu email"
+                //4 - Simplificando manipulação
+                onChange={(e) => setEmail(e.target.value)} 
+                />
             </label>
             <input type="submit" value="Enviar" />
         </form>
