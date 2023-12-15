a0ceb88116e2c27ed1757dfc19be339114495d3c
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-15 10:32:39 -0300

    Salvando dados de input em state

--- form/src/components/MyForm.jsx
+++ form/src/components/MyForm.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import "./MyForm.css";
 
 const MyForm = () => {
+    //3 - Gerenciamento de dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+       setName(e.target.value); 
+    }
+    console.log(name);
+
     return (<div>
         {/*1 - Criação de formulário */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite o seu nome" />
+                <input type="text" name="name" placeholder="Digite o seu nome"onChange={handleName}/>
             </div>
-            {/*1 - Label envolvendo input */}
+            {/*2 - Label envolvendo input */}
             <label>
                 <span>E-mail</span>
                 <input type="email" name="email" placeholder="Digite seu email" />
