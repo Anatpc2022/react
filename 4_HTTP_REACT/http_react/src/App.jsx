726fe4816b31e8a0fba619f31b24f51d5690bf2a
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-20 14:58:35 -0300

    Tratando erros

--- 4_HTTP_REACT/http_react/src/App.jsx
+++ 4_HTTP_REACT/http_react/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [products, setProducts] = useState([])
 
   // 4 - custom hook
-  const { data: items, httpConfig, loading } = useFetch(url)
+  const { data: items, httpConfig, loading, error } = useFetch(url)
 
   //  useEffect(() => {
   //    async function getData() {
@@ -55,6 +55,9 @@ function App() {
       <h1>HTTP em REACT</h1>
       {/* 6 - Loading */}
       {loading && <p>Carregando...</p>}
+
+      {/* 8 - tratando erros */}
+      {error && <p>{error}</p>}
 
       {/* 1 - resgate de dados */}
       <ul>
--- 4_HTTP_REACT/http_react/src/hooks/useFetch.jsx
+++ 4_HTTP_REACT/http_react/src/hooks/useFetch.jsx
@@ -12,6 +12,9 @@ export const useFetch = (url) => {
     // 6 - loading
     const [loading, setLoading] = useState(false)
 
+    // 8 - erros
+    const [error, setError] = useState(null)
+
     const httpConfig = (data, method) => {
 
         if (method === "POST") {
@@ -29,16 +32,21 @@ export const useFetch = (url) => {
 
     useEffect(() => {
         const fetchData = async () => {
+            // 7 - tratando erros
+            try {
+                // 6 - loading
+                setLoading(true)
 
-            // 6 - loading
-            setLoading(true)
+                const res = await fetch(url)
+                const json = await res.json()
 
-            const res = await fetch(url)
-            const json = await res.json()
+                setData(json)
+            } catch (error) {
+                console.log(error.message)
 
+                setError("Houve um erro ao carregar os dados!")
+            }
             setLoading(false)
-
-            setData(json)
         }
 
         fetchData()
@@ -69,5 +77,5 @@ export const useFetch = (url) => {
         httpRequest()
     }, [config, method, url])
 
-    return { data, httpConfig, loading }
+    return { data, httpConfig, loading, error }
 }
