2d50f75b916fe8fc1506824fd9549e302ff6147a
Anatpc2022 <ana.tpc2022@gmail.com>
2023-12-20 11:06:38 -0300

    Envio de dados

--- 4_HTTP_REACT/http_react/src/App.jsx
+++ 4_HTTP_REACT/http_react/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [products, setProducts] = useState([])
 
   useEffect(() => {
-    
+
     async function getData() {
       const res = await fetch(url)
 
@@ -20,16 +20,51 @@ function App() {
 
   }, [])
 
+  // 2 - envio de dados
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(product),
+    })
+  }
+
   return (
     <div>
-  <h1>HTTP em REACT</h1>
-  {/* 1 - resgate de dados */}
-  <ul>
-    {products.map((product) => (
-      <li key={product.id}>{product.name} - R${product.price}</li>
-    ))}
-  </ul>
-</div>
+      <h1>HTTP em REACT</h1>
+      {/* 1 - resgate de dados */}
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R${product.price}</li>
+        ))}
+      </ul>
+      {/* 2 - enviando dados */}
+      <div className="add-product">
+          <form onSubmit={handleSubmit}>
+            <label>
+              <span>Nome</span>
+              <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            </label>
+            <label>
+              <span>Preço</span>
+              <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+            </label>
+            <input type="submit" value="Enviar" />
+          </form>
+      </div>
+    </div>
   )
 }
 
